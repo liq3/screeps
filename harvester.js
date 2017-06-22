d8faf7f56c05ee568624828d768ff896ef96444c
liq3 <liquidman3@gmail.com>
2017-06-22 23:59:04 +1000

    bugfix previous commit

--- harvester.js
+++ harvester.js
@@ -1,17 +1,17 @@
 module.exports = function (creep) {
 
-	if(gathering && creep.carry.energy < creep.carryCapacity) {
+	if(creep.memory.gathering && creep.carry.energy < creep.carryCapacity) {
 		var source = creep.pos.findClosestByPath(FIND_SOURCES);
         if(source != null && creep.harvest(source) == ERR_NOT_IN_RANGE) {
             creep.moveTo(source);
 		} else if (creep.carry.energy == creep.carryCapacity) {
-			gathering = false;
+			creep.memory.gathering = false;
 		}
 	} else {
 		if(creep.transfer(Game.spawns.Spawn1,RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
 			creep.moveTo(Game.spawns.Spawn1);
 		} else if (creep.energy.carry == 0) {
-			gathering = true;
+			creep.memory.gathering = true;
 		}
 	}
 }
