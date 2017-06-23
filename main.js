8c467b3da955b278e856e585ade0769865c90bfc
liq3 <liquidman3@gmail.com>
2017-06-23 13:28:17 +1000

    tweaks and naming creeps

--- main.js
+++ main.js
@@ -3,13 +3,21 @@ var upgrader = require('upgrader');
 var builder = require('builder');
 var guard = require('guard');
 var miner = require('miner');
-var transporter = requie('transporter');
+var transporter = require('transporter');
 
 var sumCreeps = function(role) {
     return _.sum(Game.creeps, (c) => c.memory.role == role);
 }
-var createCreep = function(parts, roleStr) {
+var createCreep = function(parts, name, roleStr) {
     return Game.spawns.Spawn1.createCreep(parts, null, {role: roleStr, gathering:true});
+}
+
+var getName = function(name, num) {
+    if (!((name + num) in Game.creeps)) {
+        return name + num;
+    } else {
+        return getName(name, num+1);
+    }
 }
 
 module.exports.loop = function () {
@@ -27,18 +35,18 @@ module.exports.loop = function () {
     var numberTransporters = sumCreeps('transporter');
 
     var source = Game.spawns.Spawn1.room.find(FIND_SOURCES)[0];
-    if (numberHarvesters < 1) {
-        createCreep([WORK,WORK,CARRY,MOVE], 'harvester');
+    if (numberHarvesters < 1 && (numberMiners == 0 || numberTransporters == 0)) {
+        createCreep([WORK,WORK,CARRY,MOVE], getName('Harvester ', 0), 'harvester');
     } else if (numberMiners < 2) {
-        createCreep([WORK,WORK,CARRY,MOVE], 'miner');
-    } else if (numberTransporters < 1) {
-        createCreep([CARRY,CARRY,CARRY,MOVE,MOVE,MOVE], 'transporter');
+        createCreep([WORK,WORK,CARRY,MOVE], getName('Miner ', 0), 'miner');
+    } else if (numberTransporters < 2) {
+        createCreep([CARRY,CARRY,CARRY,MOVE,MOVE,MOVE], getName('Transporter ', 0), 'transporter');
     } else if (numberBuilders < 3) {
-        createCreep([WORK,WORK,CARRY,MOVE], 'builder');
+        createCreep([WORK,WORK,CARRY,MOVE], getName('Builder ', 0), 'builder');
     } else if (numberUpgraders < 3){
-        createCreep([WORK,WORK,CARRY,MOVE], 'upgrader');
+        createCreep([WORK,WORK,CARRY,MOVE], getName('Upgrader ', 0), 'upgrader');
     } else {
-        createCreep([WORK,WORK,CARRY,MOVE], 'builder');
+        createCreep([WORK,WORK,CARRY,MOVE], getName('Builder ', 0), 'builder');
     }
 
 	for(var name in Game.creeps) {
@@ -50,9 +58,9 @@ module.exports.loop = function () {
 		    upgrader(creep);
 		} else if (creep.memory.role == 'builder') {
             builder(creep);
-        } else if (crep.memory.role == 'miner') {
+        } else if (creep.memory.role == 'miner') {
             miner(creep);
-        } else if (crep.memory.role == 'transporter') {
+        } else if (creep.memory.role == 'transporter') {
             transporter(creep);
         }
 
