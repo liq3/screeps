37246ad414e8b527cbe894108e36b8f10636ce13
liq3 <liquidman3@gmail.com>
2017-06-25 18:12:10 +1000

    bugfix

--- attacker.js
+++ attacker.js
@@ -1,8 +1,8 @@
 module.exports = function (creep) {
     if (creep.pos.roomName == creep.memory.targetRoom) {
-        var target = creep.room.findClosestByPath(FIND_HOSTILE_SPAWNS);
+        var target = creep.pos.findClosestByPath(FIND_HOSTILE_SPAWNS);
         if (target == null) {
-            target = creep.room.findClosestByPath(FIND_HOSTILE_CREEPS);
+            target = creep.pos.findClosestByPath(FIND_HOSTILE_CREEPS);
         }
         if(target != null && creep.attack(target) == ERR_NOT_IN_RANGE) {
             creep.moveTo(target);
