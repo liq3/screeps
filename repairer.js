226772da9b481080e8a518266ae797f38dd444b5
liq3 <liquidman3@gmail.com>
2017-06-26 22:07:07 +1000

    repairer tweak

--- repairer.js
+++ repairer.js
@@ -9,7 +9,8 @@ module.exports = function (creep) {
         creep.memory.gathering = true;
     } else {
         var target = creep.pos.findClosestByPath(FIND_STRUCTURES, {filter:
-            s => s.hits < s.hitsMax && s.structureType != STRUCTURE_WALL});
+            s => s.hits < s.hitsMax && s.structureType != STRUCTURE_WALL
+		    && (s.structerType != STRUCTURE_RAMPART || s.hits < 100,000});
         if(target != null && creep.repair(target) == ERR_NOT_IN_RANGE) {
             creep.moveTo(target);
         }
