c98366d9d2940eab47f89ccb9012a7922d913921
liq3 <liquidman3@gmail.com>
2017-06-30 00:54:50 +1000

    upgraders withdraw from container

--- stationaryUpgrader.js
+++ stationaryUpgrader.js
@@ -5,5 +5,11 @@ module.exports = {
 		if(error == ERR_NOT_IN_RANGE || error == ERR_NOT_ENOUGH_ENERGY) {
 			creep.moveTo(creep.room.controller, {range: 2});
 		}
+		if (creep.carry.energy < 10) {
+			let target = creep.pos.findInRange(FIND_STRUCTURES, 1, {filter: s=>s.structureType==STRUCTURE_CONTAINER});
+			if (target.length > 0) {
+				creep.withdraw(target[0], RESOURCE_ENERGY); 
+			}
+		}
 	}
 };
