06a7d860beec2818f20dc654c0ea09574984d93d
liq3 <liquidman3@gmail.com>
2017-06-30 12:55:37 +1000

    upgrader tweaks

--- stationaryUpgrader.js
+++ stationaryUpgrader.js
@@ -1,14 +1,20 @@
 module.exports = {
 
 	run: function (creep) {
+		var target;
 		var error = creep.transfer(creep.room.controller,RESOURCE_ENERGY);
 		if(error == ERR_NOT_IN_RANGE || error == ERR_NOT_ENOUGH_ENERGY) {
-			creep.moveTo(creep.room.controller, {range: 2});
-		}
-		if (creep.carry.energy < 10) {
-			let target = creep.pos.findInRange(FIND_STRUCTURES, 1, {filter: s=>s.structureType==STRUCTURE_CONTAINER});
+			target = creep.pos.findInRange(FIND_STRUCTURES, 1, {filter: s=>s.structureType==STRUCTURE_CONTAINER});
 			if (target.length > 0) {
-				creep.withdraw(target[0], RESOURCE_ENERGY); 
+				creep.moveTo(target[0], {range: 1};
+				creep.withdraw(target[0], RESOURCE_ENERGY);
+			} else {
+				creep.moveTo(creep.room.controller, {range: 2});
+			}
+		}
+		if (creep.carry.energy < 20) {
+			if (target.length > 0) {
+				creep.withdraw(target[0], RESOURCE_ENERGY);
 			}
 		}
 	}
