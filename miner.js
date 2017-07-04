8fc5866cf07d40b58be2e71296e61768fb2ae0c4
liq3 <liquidman3@gmail.com>
2017-07-04 19:01:25 +1000

    create and build containers at source sites

--- miner.js
+++ miner.js
@@ -6,6 +6,15 @@ module.exports = {
             let containers = source.pos.findInRange(FIND_STRUCTURES, 1, {filter: s => s.structureType = STRUCTURE_CONTAINER});
             if (containers.length > 0 && creep.pos.getRangeTo(containers[0]) > 0) {
                 creep.moveTo(containers[0], {range:0});
+            } else if (containers.length == 0) {
+                let sites = source.pos.findInRange(FIND_CONSTRUCTION_SITES, 1);
+                if (sites.length == 0) {
+                    if (creep.pos.getRangeTo(source.pos) == 1) {
+                        source.room.createConstructionSite(creep.pos, STRUCTURE_CONTAINER);
+                    }
+                } else if (sites.length > 0 && creep.carry.energy > 40) {
+                    creep.build(sites[0]);
+                }
             }
             var error = creep.harvest(source);
             if (error == ERR_NOT_IN_RANGE || creep.pos.getRangeTo(source) > 1) {
