ef25ea98ec9744291d5cea32fd5864221aa38ea4
liq3 <liquidman3@gmail.com>
2017-07-06 23:42:57 +1000

    miner CPU optimization

--- miner.js
+++ miner.js
@@ -3,10 +3,18 @@ module.exports = {
  	run: function (creep) {
 		var source = Game.getObjectById(creep.memory.sourceId);
         if(source) {
-            let containers = source.pos.findInRange(FIND_STRUCTURES, 1, {filter: s => s.structureType = STRUCTURE_CONTAINER});
-            if (containers.length > 0 && creep.pos.getRangeTo(containers[0]) > 0) {
-                creep.moveTo(containers[0], {range:0});
-            } else if (containers.length == 0) {
+            let container = Game.getObjectById(creep.memory.containerId);
+            if (!container) {
+                let containers = source.pos.findInRange(FIND_STRUCTURES, 1, {filter: s => s.structureType = STRUCTURE_CONTAINER});
+                if (containers.length > 0) {
+                    container = containers[0];
+                    creep.memory.containerId = container.id;
+                }
+            }
+
+            if (container && creep.pos.getRangeTo(container) > 0) {
+                creep.moveTo(container, {range:0});
+            } else if (!container) {
                 let sites = source.pos.findInRange(FIND_CONSTRUCTION_SITES, 1);
                 if (sites.length == 0) {
                     if (creep.pos.getRangeTo(source.pos) == 1) {
@@ -16,17 +24,10 @@ module.exports = {
                     creep.build(sites[0]);
                 }
             }
-            var error = creep.harvest(source);
-            if (error == ERR_NOT_IN_RANGE || creep.pos.getRangeTo(source) > 1) {
-                creep.moveTo(source);
+            if (container && container.hits < container.hitsMax && creep.energy > 30) {
+                creep.repair(container);
             } else {
-                let structures = creep.pos.lookFor(LOOK_STRUCTURES);
-                if (structures.length > 0) {
-                    let s = structures[0];
-                    if (s instanceof StructureContainer && s.hits < s.hitsMax) {
-                        creep.repair(s);
-                    }
-                }
+                let error = creep.harvest(source);
             }
         }
 	}
