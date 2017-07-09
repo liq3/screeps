84edb9d32cad89d92465d47e85ad7620e6afb34c
liq3 <liquidman3@gmail.com>
2017-07-09 16:16:28 +1000

    typo

--- claimer.js
+++ claimer.js
@@ -1,17 +1,18 @@
 module.exports = {
 
 	run: function (creep) {
-	if (creep.memory.claimRoom != undefined) {
-		var target = creep.memory.claimRoom;
-		var action = creep.claimController;
-	} else if (creep.memory.targetRoom != undefined) {
-		var target = creep.memory.targetRoom;
-		var action = creep.reserveController;
-	}
-	if (creep.pos.roomName != target) {
-		creep.moveTo(new RoomPosition(25,25,target), {range: 22});
-		if (action(creep.room.controller) == ERR_NOT_IN_RANGE) {
-			creep.moveTo(creep.room.controller);
+		if (creep.memory.claimRoom != undefined) {
+			var target = creep.memory.claimRoom;
+			var action = creep.claimController;
+		} else if (creep.memory.targetRoom != undefined) {
+			var target = creep.memory.targetRoom;
+			var action = creep.reserveController;
+		}
+		if (creep.pos.roomName != target) {
+			creep.moveTo(new RoomPosition(25,25,target), {range: 22});
+			if (action(creep.room.controller) == ERR_NOT_IN_RANGE) {
+				creep.moveTo(creep.room.controller);
+			}
 		}
 	}
 };
