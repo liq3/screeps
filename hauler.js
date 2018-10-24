40290486a1a536c8dee33e2a4fd3658be6ee3f85
liq3 <liquidman3@gmail.com>
2018-10-25 06:51:43 +1100

    typo

--- hauler.js
+++ hauler.js
@@ -153,7 +153,7 @@ module.exports = {
 		}
 
 		if (!creep.memory.job && creep.carry.energy == creep.carryCapacity) {
-			creep.memory.job = storage
+			creep.memory.job = 'storage'
 		}
 
 		if (!creep.memory.job && creep.carry.energy < creep.carryCapacity) {
