1d3af8c12b9d60cc816c7a3ab8a09808babc628c
liq3 <liquidman3@gmail.com>
2018-10-27 12:45:43 +1100

    defineProperty(Source, 'memory'...

--- prototype_source.js
+++ prototype_source.js
@@ -1,19 +1,44 @@
-Object.defineProperty(Source.prototype, 'container', {
+Object.defineProperty(Source.prototype, 'memory', {
     get: function() {
-        if (!this._container) {
-            if (!this.memory.containerId) {
-                let container = this.pos.findInRange(FIND_STRUCTURES, 1, {structureType: STRUCTURE_CONTAINER})[0]
-                if (container) {
-                    this.memory.containerId = container.id
+        if (!this._memory) {
+            try {
+                this._memory = Memory.sources[source]
+            } catch (e) {
+                if (e typeof typeError) {
+                    Memory.sources[source] = {}
                 } else {
-                    return undefined
+                    throw e;
                 }
             }
-            this._container = Game.getObjectById(this.memory.containerId)
+        }
+        return this._memory
+    }
+});
+
+Object.defineProperty(Source.prototype, 'container', {
+    get: function() {
+        try {
             if (!this._container) {
-                delete this.memory.containerId
+                if (!this.memory.containerId) {
+                    let container = this.pos.findInRange(FIND_STRUCTURES, 1, {structureType: STRUCTURE_CONTAINER})[0]
+                    if (container) {
+                        this.memory.containerId = container.id
+                    } else {
+                        return
+                    }
+                }
+                this._container = Game.getObjectById(this.memory.containerId)
+                if (!this._container) {
+                    delete this.memory.containerId
+                }
             }
             return this._container
+        } catch (e) {
+            if (e typeof TypeError) {
+                Memory.
+            } else {
+                throw e;
+            }
         }
     }
 });
