653b85c626b9be22e7edfa88b28082e72d158ffd
liq3 <liquidman3@gmail.com>
2018-10-27 13:09:39 +1100

    get properties working

--- prototype_source.js
+++ prototype_source.js
@@ -1,15 +1,10 @@
 Object.defineProperty(Source.prototype, 'memory', {
     get: function() {
         if (!this._memory) {
-            try {
-                this._memory = Memory.sources[source]
-            } catch (e) {
-                if (e instanceof typeError) {
-                    Memory.sources[source] = {}
-                } else {
-                    throw e;
-                }
+            if (!Memory.sources[this.id]) {
+                Memory.sources[this.id] = {}
             }
+            this._memory = Memory.sources[this.id]
         }
         return this._memory
     }
@@ -17,28 +12,20 @@ Object.defineProperty(Source.prototype, 'memory', {
 
 Object.defineProperty(Source.prototype, 'container', {
     get: function() {
-        try {
-            if (!this._container) {
-                if (!this.memory.containerId) {
-                    let container = this.pos.findInRange(FIND_STRUCTURES, 1, {structureType: STRUCTURE_CONTAINER})[0]
-                    if (container) {
-                        this.memory.containerId = container.id
-                    } else {
-                        return
-                    }
-                }
-                this._container = Game.getObjectById(this.memory.containerId)
-                if (!this._container) {
-                    delete this.memory.containerId
+        if (!this._container) {
+            if (!this.memory.containerId) {
+                let container = this.pos.findInRange(FIND_STRUCTURES, 1, {structureType: STRUCTURE_CONTAINER})[0]
+                if (container) {
+                    this.memory.containerId = container.id
+                } else {
+                    return
                 }
             }
-            return this._container
-        } catch (e) {
-            if (e instanceof TypeError) {
-                Memory.
-            } else {
-                throw e;
+            this._container = Game.getObjectById(this.memory.containerId)
+            if (!this._container) {
+                delete this.memory.containerId
             }
         }
+        return this._container
     }
 });
