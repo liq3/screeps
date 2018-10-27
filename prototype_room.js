b040dbe66fd6374ff6f6e9d2bd758f573ebaba1d
liq3 <liquidman3@gmail.com>
2018-10-28 10:16:30 +1100

    it's a function not a property dah

--- prototype_room.js
+++ prototype_room.js
@@ -1,14 +1,12 @@
-Object.defineProperty(Room.prototype, 'getSourcePath', {
-    get: function(spawn, source) {
-        if (!this.memory.paths) {
-            this.memory.paths = {}
-        }
-        if (!this.memory.paths[spawn.id]) {
-            this.memory.paths[spawn.id] = {}
-        }
-        if (!this.memory.paths[spawn.id][source.id]) {
-            this.memory.paths[spawn.id][source.id] = PathFinder.search(spawn.pos, {pos:source.pos, range: 2}, {roomCallBack:global.costMatrixCallback, swamp:10, plains:2});
-        }
-        return this.memory.paths[spawn.id][source.id]
+Room.prototype.getSourcePath =  function(spawn, source) {
+    if (!this.memory.paths) {
+        this.memory.paths = {}
     }
-});
+    if (!this.memory.paths[spawn.id]) {
+        this.memory.paths[spawn.id] = {}
+    }
+    if (!this.memory.paths[spawn.id][source.id]) {
+        this.memory.paths[spawn.id][source.id] = PathFinder.search(spawn.pos, {pos:source.pos, range: 2}, {roomCallBack:global.costMatrixCallback, swamp:10, plains:2});
+    }
+    return this.memory.paths[spawn.id][source.id]
+}
