15f2d2e7028b423f7c5c1e8b8cdce3257c31bee3
liq3 <liquidman3@gmail.com>
2018-10-29 20:55:19 +1100

    more miner improvements

--- miner.js
+++ miner.js
@@ -5,15 +5,15 @@ module.exports = {
         if (creep.pos.inRangeTo(mineral, 1)) {
             if (Game.time % 5 == 0) {
                 let container = mineral.pos.findInRange(FIND_STRUCTURES, 1, {filter: {structureType:STRUCTURE_CONTAINER}})[0]
-                if (container && _.sum(container.store) < 1900) {
+                if (container && _.sum(creep.carry) + work <= creep.carryCapacity) {
                     let err = creep.harvest(mineral)
                     if (err != OK) {
                         console.log(`${creep.name} Error harvesting mineral ${err}`);
-                    }                    
+                    }
                 }
             } else if (Game.time % 5 == 1) {
                 let container = mineral.pos.findInRange(FIND_STRUCTURES, 1, {filter: {structureType:STRUCTURE_CONTAINER}})[0]
-                if (container && _.sum(container.store) < 1900) {
+                if (container && _.sum(container.store) + _.sum(creep.carry) <= container.storeCapacity) {
                     let err = creep.transfer(container, mineral.mineralType)
                     if (err != OK) {
                         console.log(`${creep.name} Error transferring minerals to container ${err}`);
