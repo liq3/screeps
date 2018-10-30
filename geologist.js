a14d43c3407d36ce1025e92046ab211f4567bb61
liq3 <liquidman3@gmail.com>
2018-10-30 14:01:05 +1100

    bugs galore

--- geologist.js
+++ geologist.js
@@ -13,27 +13,30 @@ module.exports = {
             if (!room.memory.controller) {
                 room.memory.controller = {pos:room.controller.pos}
             }
+            delete creep.memory.target
+            this.getNewTargetRoom(creep)
         }
-        this.moveTo(new RoomPosition(25,25, creep.memory.target), {range:22})
+        if (creep.memory.target) {
+            creep.moveTo(new RoomPosition(25,25, creep.memory.target), {range:22})
+        }
     },
     getNewTargetRoom: function(creep) {
         let loops = 0
         let exits = Game.map.describeExits(creep.room.name)
         let open = {}
-        for (let room of exits) {
-            open[room] = true
-        }
+        open[creep.room.name] = true
         let closed = {}
-        while (open.length) {
+        while (_.size(open)) {
             let room = _.findKey(open)
             delete open[room]
             closed[room] = true
-            if (!Memory.rooms[room].memory || !Memory.rooms[room].memory.sources) {
+            if (!Memory.rooms[room] || !Memory.rooms[room].sources) {
                 creep.memory.target = room
                 break;
             } else {
-                for (let name of Game.map.describeExits(room)) {
-                    open[name] = true
+                let exits = Game.map.describeExits(room)
+                for (let i in exits) {
+                    open[exits[i]] = true
                 }
             }
             loops++;
