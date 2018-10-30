f95c48caa100f494be297cd693b9e9ce7292d610
liq3 <liquidman3@gmail.com>
2018-10-30 22:01:10 +1100

    don't build the container at < 550 ecap

--- harvester.js
+++ harvester.js
@@ -3,20 +3,22 @@ module.exports = {
  	run: function (creep) {
 		var source = Game.getObjectById(creep.memory.sourceId);
         if (source) {
-            let container = source.container
-            if (container && creep.pos.getRangeTo(container) > 0) {
-                creep.moveTo(container, {range:0});
-            } else if (!container) {
-                if (creep.pos.getRangeTo(source) > 1) {
-                    creep.moveTo(source, {range:1});
-                } else {
-                    let sites = source.pos.findInRange(FIND_CONSTRUCTION_SITES, 1);
-                    if (sites.length === 0) {
-                        if (creep.pos.getRangeTo(source.pos) === 1) {
-                            source.room.createConstructionSite(creep.pos, STRUCTURE_CONTAINER);
+            if (creep.room.energyCapacityAvailable >= 550) {                
+                let container = source.container
+                if (container && creep.pos.getRangeTo(container) > 0) {
+                    creep.moveTo(container, {range:0});
+                } else if (!container) {
+                    if (creep.pos.getRangeTo(source) > 1) {
+                        creep.moveTo(source, {range:1});
+                    } else {
+                        let sites = source.pos.findInRange(FIND_CONSTRUCTION_SITES, 1);
+                        if (sites.length === 0) {
+                            if (creep.pos.getRangeTo(source.pos) === 1) {
+                                source.room.createConstructionSite(creep.pos, STRUCTURE_CONTAINER);
+                            }
+                        } else if (sites.length > 0 && creep.carry.energy > 30) {
+                            creep.build(sites[0]);
                         }
-                    } else if (sites.length > 0 && creep.carry.energy > 30) {
-                        creep.build(sites[0]);
                     }
                 }
             }
