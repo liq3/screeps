6c476c131348e02e14af985d13376d5d08316fc7
liq3 <liquidman3@gmail.com>
2018-11-06 20:49:55 +1100

    typo

--- spawnManager.js
+++ spawnManager.js
@@ -58,7 +58,7 @@ module.exports = {
                 let source = Game.getObjectById(entry.target)
                 let harvester = _.filter(Game.creeps, c => c.memory.sourceId === source.id && c.memory.role === 'harvester');
                 if (harvester.length === 0
-                    || (harvester.length === 1 && harvester[0].ticksToLive < ((Empire.getPathCost(firstSpawn.id, entry.target)+11)*3))) {
+                    || (harvester.length === 1 && harvester[0].ticksToLive < ((Empire.getPathCost(spawn.id, entry.target)+11)*3))) {
                     if (!(Memory.dangerRooms.includes(source.pos.roomName))) {
                         this.createCreep(spawn, 'HV', {role:'harvester',sourceId:entry.target});
                         break;
